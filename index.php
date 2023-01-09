6e90fb558f611c8db03d90dbb9182e25a431fae2
Nicolas <nicolas.gross@unimestre.com>
2023-01-09 09:17:56 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Laminas\View\Renderer\PhpRenderer;
+use Laminas\View\Resolver\TemplateMapResolver;
+
+//renderiando layout
+$view = new PhpRenderer();
+$view->setResolver(new TemplateMapResolver([
+    'layout' => __DIR__ . '/view/layout.phtml',
+    'example' => __DIR__ . '/view/example.phtml',
+]));
+
+$view->setTemplate('layout');
+
+echo $view->render('layout');
